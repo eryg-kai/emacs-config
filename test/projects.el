fb7f4beaf8f74a058fd25791110392b901821c3c
0x0049 <dev@0x0049.me>
2021-09-23 09:58:17 -0500

    [completion] Remove org from ignore project test
    Use ops instead since that will always exist.

--- test/projects.el
+++ test/projects.el
@@ -8,7 +8,7 @@
 (ert-deftest ec-test-ignore-project ()
   "Tests `ec--ignore-project'."
   (should (ec--ignore-project (expand-file-name  "~/.config/emacs")))
-  (should-not (ec--ignore-project (expand-file-name "~/org")))
+  (should-not (ec--ignore-project (expand-file-name "~/ops")))
   (should-not (ec--ignore-project (expand-file-name "~/ops/emacs")))
   (should (ec--ignore-project (expand-file-name "~/ops/emacs/config"))))
 
