c57d8dc7b7b2669dd3b7c625ee302b45aa0da7fa
0x0049 <dev@0x0049.me>
2021-10-17 16:37:10 -0500

    [exwm] Never turn off all monitors
    I do not know how this happens but sometimes xrandr reports there are no
    connected monitors so my update code ends up turning off all screens
    erroneously.

--- config/exwm.el
+++ config/exwm.el
@@ -107,6 +107,7 @@ If no ARGS are provided, prompt for the command."
         (setq ec--connected-monitors monitors)
         (when (or (bound-and-true-p ec-debug-p) (called-interactively-p))
           (message (format ">>> %s" command)))
+        (unless monitors (error "Refusing to turn off all monitors"))
         (ec-exec command)))))
 
 (defun ec--exwm-update-screens-soon ()
