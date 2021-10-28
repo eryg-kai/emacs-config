0620f625f669d7c92eb1354b2795be40d5fcb5fc
0x0049 <dev@0x0049.me>
2021-10-28 02:35:00 -0500

    [projects] Remove search path automation
    Mainly because it does not take Tramp into consideration.  Instead I
    will just set the list through customize.

--- config/projects.el
+++ config/projects.el
@@ -6,11 +6,11 @@
 
 (nconc package-selected-packages '(ripgrep
                                    projectile))
-
+;
 (setq projectile-git-submodule-command nil)
 
-;; Keymaps can't be autoloaded so here's a workaround. Loads projectile, rebinds
-;; to the keymap, then triggers the keybinding again.
+;; Keymaps cannot be autoloaded so here is a workaround.  Loads projectile,
+;; rebinds to the keymap, then triggers the keybinding again.
 (defun ec-projectile ()
   "Load projectile."
   (interactive)
@@ -21,30 +21,12 @@
 
 (define-key global-map (kbd "C-c p") #'ec-projectile)
 
-(defun ec--ignore-project (path)
-  "Return non-nil if PATH should be ignored."
-  ;; Any git repository not inside an archive or config is OK.
-  (or (string-match-p "/\\(archive\\|\\.config\\)" path)
-      (not (file-exists-p (expand-file-name ".git" path)))))
-
 (defun ec--set-repositories ()
   "Set magit repository directories from known projectile projects."
   (let ((project-dirs (bound-and-true-p projectile-known-projects)))
     (setq magit-repository-directories
           (mapcar (lambda (path) `(,path . 0)) project-dirs))))
 
-;; Do this every time to prevent issues with removing a directory then trying
-;; to search again.
-(defun ec--set-search-path ()
-  "Set the projectile search path."
-  (setq projectile-project-search-path
-        (append (list (expand-file-name "~"))
-                (cl-remove-if-not #'file-directory-p
-                                  (directory-files "~" t "^[^.]")))
-        projectile-ignored-project-function #'ec--ignore-project))
-
-(advice-add 'projectile-discover-projects-in-search-path
-            :before #'ec--set-search-path)
 (advice-add 'projectile-discover-projects-in-search-path
             :after #'ec--set-repositories)
 
--- test/projects.el
+++ test/projects.el
@@ -5,11 +5,5 @@
 
 ;;; Code:
 
-(ert-deftest ec-test-ignore-project ()
-  "Tests `ec--ignore-project'."
-  (should (ec--ignore-project (expand-file-name  "~/.config/emacs")))
-  (should-not (ec--ignore-project (expand-file-name "~/ops")))
-  (should-not (ec--ignore-project (expand-file-name "~/ops/emacs")))
-  (should (ec--ignore-project (expand-file-name "~/ops/emacs/config"))))
 
 ;;; projects.el ends here
