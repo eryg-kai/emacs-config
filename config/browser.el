417fc1d7b16bfedd6031848b2e2969ce25585a7e
0x0049 <dev@0x0049.me>
2022-02-26 15:42:00 -0600

    [browser] Use new eww-auto-rename-buffer var

--- config/browser.el
+++ config/browser.el
@@ -8,18 +8,10 @@
 
 (setq browse-url-handlers
       '(("." . eww-browse-url))
-      browse-url-secondary-browser-function #'browse-url-firefox)
-
-;; TODO: When going back it doesn't update.
-(defun ec--eww-set-buffer-title ()
-  "Set the EWW buffer title to the page's title."
-  (let* ((title  (plist-get eww-data :title))
-         (result (if (string= "" title) "*eww*" (concat "*" title "*"))))
-    (rename-buffer result t)))
-
-(add-hook 'eww-after-render-hook #'ec--eww-set-buffer-title)
-
-(setq shr-use-fonts nil
+      browse-url-secondary-browser-function #'browse-url-firefox
+      eww-auto-rename-buffer 'url
+      eww-buffer-name-length most-positive-fixnum
+      shr-use-fonts nil
       shr-max-image-proportion 0.2)
 
 ;;; browser.el ends here
