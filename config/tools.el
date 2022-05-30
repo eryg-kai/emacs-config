21b13848513890679aeee848a8b45a3a0fde3dd7
Eryg <dev@eryg.escapemetaaltcontrolshift.com>
2022-05-30 00:13:20 -0500

    [tools] Align beancount numbers with fill column

--- config/tools.el
+++ config/tools.el
@@ -130,7 +130,7 @@
 (setq bookmark-save-flag 1)
 
 ;; Beancount.
-(setq beancount-number-alignment-column 52
+(setq beancount-number-alignment-column (- fill-column 4)
       beancount-mode-map-prefix [(control c) (\')])
 (add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
 (with-eval-after-load 'evil
