bea7aa4566dcfbb11c490f1399696a028514d05a
Eryg <dev@eryg.escapemetaaltcontrolshift.com>
2022-07-18 21:53:26 -0500

    [tools] Get ERC working with SASL

--- config/tools.el
+++ config/tools.el
@@ -114,7 +114,70 @@
       erc-query-display 'buffer
       erc-auto-query 'bury)
 
+(defvar erc-sasl-server-regexp-list '()
+  "List of regexps matching server host names for which SASL should be used.")
+
+(defun erc-sasl-use-sasl-p ()
+  "Return t if SASL should be used in the current session."
+  (and (boundp 'erc-session-server)
+       (cl-loop for re in erc-sasl-server-regexp-list
+                thereis (integerp (string-match re erc-session-server)))))
+
+(with-eval-after-load 'erc-backend
+  (define-erc-response-handler (CAP)
+    "Client capability framework is used to request SASL auth, need to wait for ACK to begin" nil
+    (let ((msg (erc-response.contents parsed)))
+      (when (string-match " *sasl" msg)
+        (erc-server-send "AUTHENTICATE PLAIN")
+        ;; now wait for AUTHENTICATE +
+        )))
+
+  (define-erc-response-handler (AUTHENTICATE)
+    "Handle empty server response indicating ready to receive authentication." nil
+    (if erc-session-password
+        (let ((msg (erc-response.contents parsed)))
+          (when (string= "+" msg)
+            ;; plain auth
+            (erc-server-send
+             (format "AUTHENTICATE %s"
+                     (base64-encode-string
+                      (concat "\0" (erc-current-nick)
+                              "\0" erc-session-password) t)))))
+      (progn
+        (erc-display-message
+         parsed 'error
+         (if erc-server-connected 'active proc)
+         "You must set a password in order to use SASL authentication.")
+        ;; aborting SASL auth
+        (erc-server-send (erc-server-send "AUTHENTICATE *")))))
+
+  (define-erc-response-handler (903)
+    "Handle a successful SASL authentication." nil
+    (erc-server-send "CAP END")))
+
+(defun erc-sasl-login ()
+    "Perform user authentication at the IRC server."
+    (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+                     (erc-current-nick)
+                     (user-login-name)
+                     (or erc-system-name (system-name))
+                     erc-session-server
+                     erc-session-user-full-name))
+    (if erc-session-password
+        (erc-server-send (format "PASS %s" erc-session-password))
+      (message "Logging in without password"))
+    (when (erc-sasl-use-sasl-p) (erc-server-send "CAP REQ :sasl"))
+    (erc-server-send (format "NICK %s" (erc-current-nick)))
+    (erc-server-send
+     (format "USER %s %s %s :%s"
+             ;; hacked - S.B.
+             (if erc-anonymous-login erc-email-userid (user-login-name))
+             "0" "*"
+             erc-session-user-full-name))
+    (erc-update-mode-line))
+
 (advice-add 'erc :around #'ec-localize)
+(advice-add 'erc-login :override #'erc-sasl-login)
 
 ;; Tramp.
 (setq tramp-use-ssh-controlmaster-options nil
