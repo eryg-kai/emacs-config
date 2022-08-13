7f7504294b6f8793bef2d223488197250f9256ce
Eryg Kai <dev@eryg.escapemetaaltcontrolshift.com>
2022-08-13 14:47:42 -0500

    [theme] Fix font fallbacks
    The key was to use prepend.

--- config/theme.el
+++ config/theme.el
@@ -26,9 +26,14 @@
 
 ;; Set fonts for GUI Emacs.
 (when (display-graphic-p)
-  (set-frame-font (font-spec :family "Inconsolata Nerd Font" :size 12.0) t t)
-  (set-fontset-font t 'unicode (font-spec :name "Source Han Code JP" :size 9.0) nil)
-  (set-fontset-font t 'unicode (font-spec :name "Source Han Sans HW K" :size 9.0) nil))
+  ;; Tweak fallback font sizes so they match the line height of the main font.
+  (set-fontset-font t 'unicode (font-spec :name "Noto Color Emoji" :size 9.0) nil 'prepend)
+  (set-fontset-font t 'unicode (font-spec :name "NotoEmoji Nerd Font Mono" :size 9.0) nil 'prepend)
+  (set-fontset-font t 'unicode (font-spec :name "Source Han Code JP" :size 9.0) nil 'prepend)
+  (set-fontset-font t 'unicode (font-spec :name "Source Han Sans HW K" :size 9.0) nil 'prepend))
+
+;; Main font.
+(add-to-list 'default-frame-alist '(font . "Inconsolata Nerd Font"))
 
 ;; Theme overrides.
 (defvar ec-themes '(doom-one doom-solarized-light) "List of themes to cycle between.")
