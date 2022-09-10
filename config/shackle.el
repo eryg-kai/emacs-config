b9ef01af8166446ad5566ab81e932094ae4e4f05
Eryg Kai <dev@eryg.escapemetaaltcontrolshift.com>
2022-09-10 01:44:58 -0500

    [shackle] Zoom buffer names changed again

--- config/shackle.el
+++ config/shackle.el
@@ -125,7 +125,7 @@ Options are:
                  (display-buffer-at-bottom) :height 0.3)
 
                 ;; Display floating.
-                (("^\\*.zoom \\*$") (display-buffer-no-window) :float t)
+                (("^\\*[zZ]oom\\*$") (display-buffer-no-window) :float t)
 
                 (("^\\*Dictionary\\*")
                  (display-buffer-in-direction) :only t :width 0.5)
