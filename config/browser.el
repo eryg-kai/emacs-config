96058c8b6ab00db39f2c2fb61414039158eac860
Eryg Kai <dev@eryg.komyu.net>
2023-06-15 14:38:15 -0800

    [browser] Add alist for transforming URLs

--- config/browser.el
+++ config/browser.el
@@ -14,4 +14,19 @@
       shr-use-fonts nil
       shr-max-image-proportion 0.2)
 
+(defcustom ec-url-transforms nil "Pairs of replacements to perform on URLs."
+  :type '(alist :key-type string :value-type string)
+  :group 'eww)
+
+(defun ec--transform-url (url)
+  "Transform URL."
+  (seq-reduce
+   (lambda (result pair)
+     (string-replace (car pair) (cdr pair) result))
+   ec-url-transforms
+   url))
+
+(with-eval-after-load 'eww
+  (push #'ec--transform-url eww-url-transformers))
+
 ;;; browser.el ends here
